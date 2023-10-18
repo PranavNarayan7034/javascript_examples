c3fe7ab6a28c46d719527d987ed4cee18060eabb
PranavNarayan7034 <139863209+PranavNarayan7034@users.noreply.github.com>
2023-10-18 15:31:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Dom -- document object model
+
+// 1)calling a tag with their id --- getelementid 
+
+// var a= document.getElementById('myh1')
+
+// a.innerHTML='hi javascript'
+// a.style.color='blue'
+
+//*********** change content while clicking button *********
+
+// var a=document.getElementById('myh1')
+
+// function change(){
+//     a.innerHTML='learn javascript'
+//     a.style.color='blue'
+// }
+
+
+
+
+// *********************************
+var a=document.getElementById('mydiv')
+
+function imageview(){
+    a.style.display='block'
+}
+
+
+function imageclose(){
+    a.style.display='none'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
