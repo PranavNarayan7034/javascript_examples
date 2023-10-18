c8abec69799442f4673936b8308f0dbf375d28cf
PranavNarayan7034 <139863209+PranavNarayan7034@users.noreply.github.com>
2023-10-18 16:10:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,6 +33,27 @@ function imageclose(){
 
 
 
+readmore and readless javascript
+
+
+var a= document.getElementById('para2')
+var b=document.getElementById('btn1')
+var c=document.getElementById('btn2')
+
+
+function readmore(){
+    a.style.display='inline'
+    b.style.display='none'
+    c.style.display='inline'
+}
+
+function readless(){
+    a.style.display='none'
+    b.style.display='inline'
+    c.style.display='none'
+}
+
+
 
 
 
